cb2e517efa0d8bcefae52076e6034664dbdca2ac
Toki321 <65564514+Toki321@users.noreply.github.com>
2022-04-26 23:53:06 +0200

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.13;
 
+contract HelloWorld {
+
+    uint256 _number;
+
+    constructor () {}
+
+    // function to store the number
+    function storeNumber(uint256 number) public {
+        _updateNumber(number);
+    }
+
+    function _updateNumber(uint256 _newNumber) internal {
+        _number = _newNumber;
+    }
+
+    // function to retrieve the number
+    function retrieveNumber() public view returns(uint256) {
+        return _number;
+    }
+}
